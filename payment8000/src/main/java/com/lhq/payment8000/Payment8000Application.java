12719253b7a247ecdae1d19302e0b7b1aa85fe53
lianghanqiang <414464732@qq.com>
2022-07-23 07:44:08 +0800

    将cloudconsumerorder80与payment8000入驻Eureka

--- payment8000/src/main/java/com/lhq/payment8000/Payment8000Application.java
+++ payment8000/src/main/java/com/lhq/payment8000/Payment8000Application.java
@@ -4,10 +4,12 @@ import com.lhq.payment8000.controller.PaymentController;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class Payment8000Application {
 
     public static void main(String[] args) {
